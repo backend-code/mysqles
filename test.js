c8dc7ca013ba162784a3d4b45124bf837e72ac0d
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-08-28 16:17:35 +0700

    update

--- test.js
+++ test.js
@@ -4,9 +4,11 @@ mysqlse.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'blackboard_base'
+    // database: 'blackboard_base'
 },{useNewUrlParser: true})
 
-const db = mysqlse.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-// console.log(db);
+const db = mysqlse.connection.on('error', cb =>{
+    console.log(cd);
+});;
+// db.on('error', console.error.bind(console, 'connection error:'));
+console.log(mysqlse);
